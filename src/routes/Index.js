92ca808dc8fad16396620aecfbca1dc9306406b9
EloraBarua <elorabarua62@gmail.com>
2022-07-26 19:16:04 +0600

    app initialize

---
+++ src/routes/Index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Routes } from 'react-router-dom';
+
+const Index = () => {
+    return (
+        <div>
+            <Routes>
+                
+            </Routes>
+        </div>
+    );
+};
+
+export default Index;
